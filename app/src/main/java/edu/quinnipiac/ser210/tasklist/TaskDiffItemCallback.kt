da8f4b018aba91cfc51f23ae73c82df1ba99c7d2
Emilio Cruz <eecruz@quinnpiac.edu>
2023-05-08 15:16:40 -0400

    chapter 16

---
+++ app/src/main/java/edu/quinnipiac/ser210/tasklist/TaskDiffItemCallback.kt
@@ -0,0 +1,11 @@
+package edu.quinnipiac.ser210.tasklist
+
+import androidx.recyclerview.widget.DiffUtil
+
+class TaskDiffItemCallback : DiffUtil.ItemCallback<Task>()
+{
+    override fun areItemsTheSame(oldItem: Task, newItem: Task)
+            = (oldItem.taskId == newItem.taskId)
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task) = (oldItem == newItem)
+}
--- app/src/main/java/edu/quinnipiac/ser210/tasklist/TasksFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/tasklist/TasksFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import edu.quinnipiac.ser210.tasklist.databinding.FragmentTasksBinding
 
@@ -24,6 +26,17 @@ class TasksFragment : Fragment()
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+
+        val adapter = TaskItemAdapter{taskId ->
+            Toast.makeText(context, "Clicked task $taskId", Toast.LENGTH_SHORT).show()
+        }
+        binding.tasksList.adapter = adapter
+
+        viewModel.tasks.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
 
         return binding.root
     }
